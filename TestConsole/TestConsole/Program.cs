57e3f1b1f4f393cdbc15c79deb52b73d46705888
akruhoberec1 <akruhoberec1@outlook.com>
2023-05-23 15:21:33 +0200

    Fixing and updating

--- TestConsole/TestConsole/Program.cs
+++ TestConsole/TestConsole/Program.cs
@@ -10,91 +10,85 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Author firstAuthor = new Author();
-            Console.WriteLine("\nHello dear user, input a new Author:\n");
-            Console.WriteLine("\nFirst name:\n");
-            firstAuthor.FirstName = Console.ReadLine();
-            Console.WriteLine("\nLast name:\n");
-            firstAuthor.LastName = Console.ReadLine();
-            Console.WriteLine("\nAlias:\n");
-            firstAuthor.Alias = Console.ReadLine();
-
-            Console.WriteLine($"\nYou just entered an author called { firstAuthor.FirstName} { firstAuthor.LastName} and his alias is { firstAuthor.Alias} \n" );
-            Console.ReadLine();
-            Console.WriteLine("\nThe author can now write a new article:\n");
-            Console.ReadLine();
-            Article newArticle = new Article();
-            Category category = new Category();
-            Console.WriteLine("\nWhat category does the article belong to?\n");
-            category.Name = Console.ReadLine();
-            Console.WriteLine("\nGreat! What will be the title?\n");
-            newArticle.Title = Console.ReadLine();
-            Console.WriteLine("\nWrite a short description\n");
-            newArticle.Description = Console.ReadLine();
-            Console.WriteLine("\nWrite the body text:");
-            newArticle.Text = Console.ReadLine();
-
-            
-            
 
 
+            List<Category> categories = new List<Category>
+            {
+                new Category { Id = Guid.NewGuid(), Name = "Technology"},
+                new Category { Id = Guid.NewGuid(), Name = "Lifestyle"},
+                new Category { Id = Guid.NewGuid(), Name = "AutoMoto"},
+                new Category { Id = Guid.NewGuid(), Name = "World"},
+                new Category { Id = Guid.NewGuid(), Name = "Region"},
+                new Category { Id = Guid.NewGuid(), Name = "Culture"},
 
-            //List<Category> categories = new List<Category>
-            //{
-            //    new Category { Id = Guid.NewGuid(), Name = "Technology"},
-            //    new Category { Id = Guid.NewGuid(), Name = "Lifestyle"},
-            //    new Category { Id = Guid.NewGuid(), Name = "AutoMoto"},
-            //    new Category { Id = Guid.NewGuid(), Name = "World"},
-            //    new Category { Id = Guid.NewGuid(), Name = "Region"},
-            //    new Category { Id = Guid.NewGuid(), Name = "Culture"},
-
-            //};
-
-
-            //int articleNo = 1;
-            //List<Article> articles = new List<Article> { };
+            };
 
 
-            //for (int i = 0; i < 20; i++)
-            //{
-            //    articles.Add(new Article { Id = Guid.NewGuid(), Title = "Title " + articleNo, Description = "Description " + articleNo, Text = "Lorem ipsum something " + articleNo, Category = categories.FirstOrDefault(c => c.Name.Equals("")).Id });
-            //}
+            int articleNo = 1;
+            List<Article> articles = new List<Article> { };
 
 
-            //string editor = "Editor";
-            //string author = "Author";
-            //Console.WriteLine("\nHello dear user!\n");
-            //Console.WriteLine("\nType in Editor if you're an editor, Author if you're an author:\n");
-            //string compareString = Console.ReadLine();
+            for (int i = 0; i < 20; i++)
+            {
+                articles.Add(new Article { Id = Guid.NewGuid(), Title = "Title " + articleNo, Description = "Description " + articleNo, Text = "Lorem ipsum something " + articleNo, Category = categories.FirstOrDefault(c => c.Name.Equals("")).Id });
+            }
 
-            //if(compareString == editor)
-            //{
-            //    Console.WriteLine("\n Hello editor! Please select a category you want to see articles from, or press 0 to insert a new Author:\n");
-            //    int num = 1;
-            //    foreach (var cat in categories)
-            //    {
 
-            //        Console.Write(num); Console.WriteLine(". " + cat.Name);
+            string editor = "Editor";
+            string author = "Author";
+            Console.WriteLine("\nHello dear user!\n");
+            Console.WriteLine("\nType in Editor if you're an editor, Author if you're an author:\n");
+            string compareString = Console.ReadLine();
 
-            //        num++;
-            //    }
 
-            //    Console.Read();
-            //}if(compareString == author)
-            //{
-            //    Console.WriteLine("\n Hello author! Select a category to write a new article: \n");
-            //    int num = 1;
-            //    foreach (var cat in categories)
-            //    {
+            //EDITOR PART
+            if (compareString == editor)
+            {
+                Console.WriteLine("\n Hello editor! Please select a category you want to see articles from, or press 0 to insert a new Author:\n");
+                int num = 1;
+                foreach (var cat in categories)
+                {
 
-            //        Console.WriteLine(num);
-            //        Console.WriteLine(cat.Name);
-            //        num++;
-            //    }
+                    Console.Write(num); Console.WriteLine(". " + cat.Name);
+
+                    num++;
+                }
+
+                Console.Read();
+            }
 
 
 
-            //}
+
+            //AUTHOR PART
+
+            if (compareString == author)
+            {
+                Console.WriteLine("\n Hello author! Select a category to write a new article: \n");
+                int num = 1;
+                foreach (var cat in categories)
+                {
+
+                    Console.WriteLine(num);
+                    Console.WriteLine(cat.Name);
+                    num++;
+                }
+
+
+
+            }
+            else
+            {
+                Console.WriteLine("\nHello user, you can read articles, here's a list of categories so you can pick desired topic:\n");
+                int num = 1;
+                foreach (var cat in categories)
+                {
+
+                    Console.WriteLine(num);
+                    Console.WriteLine(cat.Name);
+                    num++;
+                }
+            }
 
 
             Console.Read();
