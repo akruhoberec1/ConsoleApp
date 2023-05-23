b4f7d32d233eecb1915a8b09f07771a0bcc442c6
Andrija Kruhoberec <akruhoberec1@outlook.com>
2023-05-23 13:49:36 +0200

    Add files via upload

---
+++ TestConsole/TestConsole/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        public Category Category { get; set; }
+        public Author Author { get; set;  }
+        public DateTime PostTime { get; set; } = DateTime.UtcNow;
+
+    }
+}
---
+++ TestConsole/TestConsole/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Author : Person
+    {
+        public string Alias { get; set; }
+
+        
+    }
+
+}
